cddc2674990209b6d6b1cdcbe10d12c6aca41cdc
Jay Maas <jaymaas.business@gmail.com>
2019-04-09 15:13:06 -0600

    First Commit
    Updated 2 stylings- Padding 2% on the whole container for more responsive design and opacity on the fixed navbar.
    
    Mouseover turns nav items orange when the mouse goes over!

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+//mouseover
+let number1 = document.querySelector(".main-navigation");
+number1.addEventListener("mouseover", function( event ) {   
+    // highlight the mouseover target
+    event.target.style.color = "orange";
+setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  }, false);
+
+  
