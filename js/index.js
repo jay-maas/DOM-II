74851c27747ba46c30b946354f6d0b091986272a
Jay Maas <jaymaas.business@gmail.com>
2019-04-09 16:46:19 -0600

    Drive the Bus
    I am still working to get the bus to drive across my screen. Commit for pull for stand UP.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
 //mouseover
-let number1 = document.querySelector(".main-navigation");
-number1.addEventListener("mouseover", function( event ) {   
-    // highlight the mouseover target
+let over1 = document.querySelector(".logo-heading"); 
+over1.addEventListener("mouseover", function( event ) {   
     event.target.style.color = "orange";
 setTimeout(function() {
       event.target.style.color = "";
     }, 500);
-  }, false);
+}, false);
 
-  
+let over2 = document.querySelector(".nav"); 
+over2.addEventListener("mouseover", function( event ) {   
+    event.target.style.color = "orange";
+setTimeout(function() {
+    event.target.style.color = "";
+    }, 500);
+}, false);
+  //keydown
+  alert("If you push keys on my page it will do stuff! Have FUN with FUNBUS!!!");
