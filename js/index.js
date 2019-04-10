3b47b1068bf0e979745ea6f4d03e881840bf2bd5
Jay Maas <jaymaas.business@gmail.com>
2019-04-09 21:58:58 -0600

    Two more objectives
    The bus can be driven now! Also displays a load message.

--- js/index.js
+++ js/index.js
@@ -14,5 +14,51 @@ setTimeout(function() {
     event.target.style.color = "";
     }, 500);
 }, false);
-  //keydown
-  alert("If you push keys on my page it will do stuff! Have FUN with FUNBUS!!!");
+
+//keydown
+let bus = document.querySelector(".bus");
+let container = document.querySelector(".bus-container");
+
+let busLeft = 0;
+let busDown = 100;
+
+
+document.addEventListener('keydown', function move(e) {
+    if (e.keyCode === 68) {
+        busLeft +=20;
+        bus.style.left = busLeft + "px";
+        if(busLeft >= 600) {
+            busLeft -=20;
+        } 
+    }
+    if (e.keyCode === 65) {
+        busLeft -=20;
+        bus.style.left = busLeft + "px";
+        if(busLeft <=0){
+            busLeft +=20;
+        }
+    }
+    if (e.keyCode === 83) {
+        busDown +=20;
+        bus.style.top = busDown + "px";
+        if(busDown >= 1700) {
+            busDown -=20;
+        }
+    }
+    if (e.keyCode === 87) {
+        busDown -=20;
+        bus.style.top = busDown + "px";
+        if(busDown <= 100) {
+            busDown +=20;
+        }
+    }
+
+    
+    });
+
+    //Load
+    window.addEventListener('load', (event) => {
+        alert('Enjoy the Fun Bus. Use WASD to drive the BUS!');
+    });
+
+
